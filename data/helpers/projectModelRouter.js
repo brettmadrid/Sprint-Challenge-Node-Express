8cbb27f6d1ddfc468f6d4533f0f82a1e05ee6255
brettmadrid <brettmadrid9@gmail.com>
2019-03-10 18:22:48 -0700

    Set up get endpoint for /api/projects

--- data/helpers/projectModelRouter.js
+++ data/helpers/projectModelRouter.js
@@ -6,4 +6,15 @@ const Projects = require("./projectModel.js");
 const router = express.Router();
 //router.use(cors());
 
+router.get("/", async (req, res) => {
+  try {
+    const projects = await Projects.get();
+    res.status(200).json(projects);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error retrieving projects"
+    });
+  }
+});
+
 module.exports = router;
